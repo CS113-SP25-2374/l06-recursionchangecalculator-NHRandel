fce9ab60e33630f7cce0efede6ff0d3f61f3c7d3
noahrandel <Nhrandel89@gmail.com>
2025-03-17 15:49:37 -0700

    donezo

--- src/edu/miracosta/cs113/change/ChangeCalculatorRecursive.java
+++ src/edu/miracosta/cs113/change/ChangeCalculatorRecursive.java
@@ -3,30 +3,51 @@ package edu.miracosta.cs113.change;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * ChangeCalculator : Class containing the recursive method calculateChange, which determines and prints all
- * possible coin combinations representing a given monetary value in cents.
- *
- * Problem derived from Koffman & Wolfgang's Data Structures: Abstraction and Design Using Java (2nd ed.):
- * Ch. 5, Programming Project #7, pg. 291.
- *
- * NOTE: An additional method, printCombinationsToFile(int), has been added for the equivalent tester file to
- * verify that all given coin combinations are unique.
- */
 public class ChangeCalculatorRecursive extends ChangeCalculator {
 
     /**
-     * Wrapper method for determining all possible unique combinations of quarters, dimes, nickels, and pennies that
-     * equal the given monetary value in cents
+     * Recursive method to calculate all unique combinations of coins that sum up to the given amount.
      *
-     * @param cents a monetary value in cents
-     * @return the total number of unique combinations of coins of which the given value is comprised
+     * @param cents the amount in cents
+     * @return a list of all unique combinations of coins
      */
     public static List<String> calculateRecursive(int cents) {
-        // TODO:
-        // Implement a recursive solution following the given documentation.
         List<String> combinations = new ArrayList<>();
-
+        calculateRecursiveHelper(cents, 0, 0, 0, cents, combinations);
         return combinations;
+    }
+
+    /**
+     * Helper method for recursive calculation of coin combinations.
+     *
+     * @param cents     the remaining amount in cents
+     * @param quarters  the number of quarters used so far
+     * @param dimes     the number of dimes used so far
+     * @param nickels   the number of nickels used so far
+     * @param pennies   the number of pennies used so far
+     * @param combinations the list to store valid combinations
+     */
+    private static void calculateRecursiveHelper(int cents, int quarters, int dimes, int nickels, int pennies, List<String> combinations) {
+
+        if (cents == 0) {
+            combinations.add(combinationToString(quarters, dimes, nickels, pennies));
+            return;
+        }
+
+        if (cents >= COINS[Quarter]) {
+            calculateRecursiveHelper(cents - COINS[Quarter], quarters + 1, dimes, nickels, pennies, combinations);
+        }
+
+        if (cents >= COINS[Dime]) {
+            calculateRecursiveHelper(cents - COINS[Dime], quarters, dimes + 1, nickels, pennies, combinations);
+        }
+
+        if (cents >= COINS[Nickel]) {
+            calculateRecursiveHelper(cents - COINS[Nickel], quarters, dimes, nickels + 1, pennies, combinations);
+        }
+
+        if (cents >= COINS[Penny]) {
+            calculateRecursiveHelper(cents - COINS[Penny], quarters, dimes, nickels, pennies + 1, combinations);
+        }
     }
 }
